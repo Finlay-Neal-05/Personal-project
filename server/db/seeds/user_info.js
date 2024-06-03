2208a6bac952bfcee85c04f1d9da14da3b9250b6
Finlay Neal <finlayhormann@gmail.com>
2024-06-03 17:00:45 +1200

    create seed data for user_info

--- server/db/seeds/user_info.js
+++ server/db/seeds/user_info.js
@@ -4,10 +4,10 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('user_info').del()
+  await knex('user_info').insert([
+    {id: 1, user_id: 1, goals_completed: 1, goals_failed: 1},
+    {id: 2, user_id: 2, goals_completed: 2, goals_failed: 2},
+    {id: 3, user_id: 3, goals_completed: 3, goals_failed: 3}
   ]);
 };
