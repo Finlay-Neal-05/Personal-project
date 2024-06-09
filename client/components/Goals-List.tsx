5efe8aea5fd3261471e12500de04dce246e3eefe
finlay neal <finlayhormann@gmail.com>
2024-06-09 21:32:29 +1200

    conditional render goals List

--- client/components/Goals-List.tsx
+++ client/components/Goals-List.tsx
@@ -1,10 +1,8 @@
 import { useGoals } from '../hooks/useGoals'
 import { GoalData } from '../../models/goals'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 export default function GoalsList() {
-  const goalsList = useGoals()
-  console.log(goalsList)
-
   const { data, isPending, isError, error } = useGoals()
 
   const handleClick = () => {
@@ -22,19 +20,28 @@ export default function GoalsList() {
 
   return (
     <>
-      <div className="goalContainer">
-        <p>This is a test</p>
-        {data.map((goal: GoalData, i: number) => {
-          return (
-            <p key={i}>
-              {goal.title}, {goal.content}, id: {goal.id}
-            </p>
-          )
-        })}
-        <button className="button1" id="addGoal" onClick={handleClick}>
-          Add Goal
-        </button>
-      </div>
+      <IfAuthenticated>
+        <>
+          <div className="goalContainer">
+            <p>This is a test</p>
+            {data.map((goal: GoalData, i: number) => {
+              return (
+                <p key={i}>
+                  {goal.title}, {goal.content}, id: {goal.id}
+                </p>
+              )
+            })}
+            <button className="button1" id="addGoal" onClick={handleClick}>
+              Add Goal
+            </button>
+          </div>
+        </>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <div className="goalContainer">
+          <p>please login to see goals</p>
+        </div>
+      </IfNotAuthenticated>
     </>
   )
 }
